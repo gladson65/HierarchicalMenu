263dcb9be912e7bb00f531dec77b36da4db9ea8f
gladson65 <grauth65@gmail.com>
2025-10-07 17:15:30 +0530

    initial cleaning, setup and dummy data added

---
+++ src/utils/data.js
@@ -0,0 +1,30 @@
+export const menuItems = [
+    {
+        id: 1,
+        label: 'Fruits',
+        children: [
+            { id:1.1, label: 'Apples' },
+            { id:1.2, label: 'Oranges' },
+        ]
+    },
+
+    {
+        id: 2,
+        label: 'Vegetables',
+        children: [
+            { id:2.1, label: 'Carrots' },
+            { id:2.2, label: 'Potatos' },
+        ]
+    },
+
+    {
+        id: 3,
+        label: 'Diary',
+        children: [
+            { id:3.1, label: 'Milk' },
+            { id:3.2, label: 'Chocolates' },
+        ]
+    },
+
+    
+]
